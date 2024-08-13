544df4f8fc2cc2535e6f46755acc40fca824f9f5
Mustafa Cenan <54mustafacenan54@gmail.com>
2024-08-13 03:37:25 +0300

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,29 @@
+﻿using BuilderPattern.Method1;
+using System.Text;
+
+
+
+
+
+
+//var sb = new StringBuilder();
+
+//sb.Append("Mustafa");
+//sb.Append("Cenan");
+
+//var fullname = sb.ToString();
+
+var baseURL = "https://localhost:8080";
+
+var eb = new EndpointBuilder(baseURL);
+
+eb
+    .Append("api")
+    .Append("v1")
+    .Append("user")
+    .AppendParam("id", "5")
+    .AppendParam("username", "5");
+ 
+
+var a = eb.Build().ToString();
+Console.WriteLine(a);
